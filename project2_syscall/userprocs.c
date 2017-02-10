b1d9d5db4c663e4ef2bcfd250f948c7a9fa9ef45
joseph <josephthweatt@gmail.com>
2017-02-10 12:15:37 -0700

    trying to implement something in userprocs that uses copy_to_user(). Need to find a way to check it

--- project2_syscall/userprocs.c
+++ project2_syscall/userprocs.c
@@ -1,34 +1,46 @@
 #include "userprocs.h"
+
+#include <asm/uaccess.h>
 #include <linux/cred.h>
 #include <linux/syscalls.h>
 #include <linux/sched.h>
 #include <linux/kernel.h>
 
 #include <string.h>
-#include <stdio.h>
 
 // we need to see if and how we can pass a custom stucture to a syscall
 asmlinkage long sys_userprocs (task_struct **taskptr) {
 
+	unsigned int size = 0;
+	unsigned int proc_size = sizeof(struct user_proc);
+	struct user_proc *procs_head, *proc;
 	struct task_struct *task = current;
 
+	proc = procs_head;
 	
 	for_each_process(task) {
 
 		if (current_uid() == task->cred->uid) {
 			int time_in_seconds = task->utime / HZ;
 
-//			printf("%s\t%d\t%s\t%d\t%s\n", 
-//				uname, task->pid, task->signal->tty,
-//				time_in_seconds, task->comm);
 			// assign the values in task to the struct defined in userprocs.h
 			// then add it to the linked list
-			
+			proc = (struct user_proc*) malloc(proc_size);
+			proc->pid = task->pid;
+			proc->tty = task->signal->tty;
+			proc->time_in_seconds = time_in_seconds;
+			proc->comm = task->comm;
 
+			proc = proc->next;
+			size += proc_size;
 		}
 	}
 	// we need to copy the task list to the userspace, I think this is the copy_to_user
-	// function, but I'm fuzzy on the implementation 
+	// function, but I'm fuzzy on the implementation
+
+	if (copy_to_user(*taskptr, task, size)) {
+		return -EFAULT;
+	}
 
 	return 0;
 }
